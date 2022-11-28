7d24645f2d724a3d7aa5513f56bf4bc36644079e
sapje1 <evansj31@cardiff.ac.uk>
2022-11-28 16:12:12 +0000

    clinical xnat scripts (for reference)

---
+++ ClinicalXNAT.sh
@@ -0,0 +1,10 @@
+# Copy and run the following command in the Terminal:
+ssh -XY $USER@vlan999.l072 "firefox &"
+
+# Alternatively, run the following:
+cd Desktop
+./ClinicalXNAT.sh
+
+# When firefox starts, go to http://clinicalxnat.cubric.cf.ac.uk
+
+# Login using the 'radiographer' account
---
+++ startclinicalxnat.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+ssh -X 10.186.157.194
+google-chrome clinicalxnat.cubric.cf.ac.uk
+
